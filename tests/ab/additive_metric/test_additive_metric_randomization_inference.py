2e254d6e69c077b0a58e713fe3ec3915ea5a91c2
Patrick Le <le.chi.patrick@gmail.com>
2022-12-15 15:55:51 -0600

    test

--- tests/ab/additive_metric/test_additive_metric_randomization_inference.py
+++ tests/ab/additive_metric/test_additive_metric_randomization_inference.py
@@ -60,7 +60,7 @@ class TestRandomizationInference:
         threshold to reject the null, then the fraction of such rejections should be around 5 percent
         """
         # generate the p-values
-        num_sims = 100  # 00
+        num_sims = 1000
         pvalues = np.zeros((num_sims,))
 
         _rv = np.random.normal(0, 1, size=(2000,))
